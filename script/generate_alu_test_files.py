d6bc3a85550ab9b860132665bc49f35135e43bf9
Mohamud Mohamud <moh2169407@maricopa.edu>
2025-09-20 14:36:29 -0700

    generating alu tests from test.yaml file

---
+++ script/generate_alu_test_files.py
@@ -0,0 +1,66 @@
+import sys
+import yaml
+
+try:
+    with open ("../tests/test_alu.yaml", "r") as yamlFile:
+        data = yaml.safe_load(yamlFile)
+except FileNotFoundError:
+    print("ERROR: ALU test yaml file doesn't exisit") 
+    sys.exit(1)
+
+with open ("../tests/test_alu_gen.c" , "w") as outFile:
+    outFile.write("#include \"alu.h\"\n")
+    outFile.write("#include \"log.h\"\n")
+    outFile.write("#include <stdbool.h>\n")
+    outFile.write("\n\n")
+
+    outFile.write("bool isFailing = false;\n")
+    outFile.write("\n")
+
+    # Iterate through the tests inside data
+    for tests in data:
+        name = tests.get("Name") or None
+        fn = tests.get("fn") or None
+        inputType = tests.get("inType")
+        outType = tests.get("outType")
+        cases = tests.get("test_cases") or []
+        checkOverFlow = tests.get("checkOverFlow") or None
+        checkUnderFlow = tests.get("checkUnderFlow") or None
+        checkSwap = tests.get("checkSwap");
+
+
+        outFile.write(f"void {name}() {{\n")
+        outFile.write("\t// stores the alu outputs\n")
+        outFile.write("    OperandFormat format;\n")
+        outFile.write(f"    {outType} x = 0;\n")
+        outFile.write("    format.data = &x;\n");
+
+        outFile.write(f"    {inputType} source1;\n")
+        outFile.write(f"    {inputType} source2;\n")
+
+        i = 0
+        for case in cases:
+            outFile.write("\n")
+            outFile.write(f"    // test case {i}:\n")
+            outFile.write(f"    source1 = {case[0]};\n")
+            outFile.write(f"    source2 = {case[1]};\n")
+
+            outFile.write(f"    {fn}(&source1, &source2, &format);\n")
+
+            outFile.write(f"    if (*({outType}*)format.data != {case[2]}) {{\n")
+            outFile.write(f"        log_trace(\"{name} test case: {i} failed ❌\");\n")
+            outFile.write(f"        isFailing = true;\n")
+            outFile.write(f"    }}\n    else{{\n")
+            outFile.write(f"        log_trace(\"{name} (test case: {i}) passed ✅\");\n")
+            outFile.write("    }\n")
+
+
+            i += 1
+        outFile.write("}\n\n\n");
+
+    outFile.write("int main(void) {\n")
+    for i in data:
+        name = i.get("Name")
+        outFile.write(f"    {name}();\n") 
+
+    outFile.write("}\n")
--- tests/test_alu_gen.c
+++ tests/test_alu_gen.c
@@ -6,24 +6,48 @@
 bool isFailing = false;
 
 void alu_add_test() {
-    // stores the alu outputs
+	// stores the alu outputs
     OperandFormat format;
-    int x = 0;
+    int64_t x = 0;
     format.data = &x;
-    int source1;
-    int source2;
+    int64_t source1;
+    int64_t source2;
 
+    // test case 0:
     source1 = 1;
     source2 = 5;
     handle_add_fn(&source1, &source2, &format);
+    if (*(int64_t*)format.data != 6) {
+        log_trace("alu_add_test test case: 0 failed ❌");
+        isFailing = true;
+    }
+    else{
+        log_trace("alu_add_test (test case: 0) passed ✅");
+    }
 
+    // test case 1:
     source1 = 100;
     source2 = 300;
     handle_add_fn(&source1, &source2, &format);
+    if (*(int64_t*)format.data != 400) {
+        log_trace("alu_add_test test case: 1 failed ❌");
+        isFailing = true;
+    }
+    else{
+        log_trace("alu_add_test (test case: 1) passed ✅");
+    }
 
+    // test case 2:
     source1 = 11231;
     source2 = 120120;
     handle_add_fn(&source1, &source2, &format);
+    if (*(int64_t*)format.data != 131351) {
+        log_trace("alu_add_test test case: 2 failed ❌");
+        isFailing = true;
+    }
+    else{
+        log_trace("alu_add_test (test case: 2) passed ✅");
+    }
 }
 
 
